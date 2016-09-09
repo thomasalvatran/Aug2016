611b7ae59515922a9611d52dde8e9f2771f11ba7
thomasalvatran <thomasalvatran@gmail.com>
2016-09-08 21:31:18 -0400

    Aug2016

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,7 +1,11 @@
+//----------------------/home/tovantran/Ctest/Dijkstra.cpp
 #include <iostream>
 #include <queue>
 #include <map>
 #include <algorithm>
+#include <functional>
+#include <unordered_map>
+
 using namespace std;
 
 class DistanceToCity
@@ -26,7 +30,7 @@ public:
     void setDistance(string from, string to, int distance)
     {
         cities.insert(make_pair(from, DistanceToCity(to, distance)));
-    }
+    }                                                                       /*pass as ref*/
     priority_queue<DistanceToCity> findDistance(string start, map<string, int> &shortest)
     {
         priority_queue<DistanceToCity> que, que1;
@@ -67,6 +71,14 @@ for_each(d.cities.begin(), d.cities.end(),[](pair<string, DistanceToCity> i){
 });
 }
 //std::for_each(begin(v), end(v), [&sum](int x) { sum += x; });
+//auto comp = []( const int& a, const int& b ){return a.size > b.size;};
+template <typename T1, typename T2> //Functor for a pair
+struct less_second {
+    typedef pair<T1, T2> type; //????Review???? September 08 2016 (Thursday)
+    bool operator ()(type const& a, type const& b) const {
+        return a.second < b.second;
+    }
+};
 
 int main()
 {
@@ -86,6 +98,7 @@ int main()
     print(d); printf("%s\n","-----" );
     //1
     map<string, int> shortest;
+
     priority_queue<DistanceToCity> pq = d.findDistance("Pierre", shortest);
     while(!pq.empty())
     {
@@ -93,7 +106,12 @@ int main()
         pq.pop();
         cout << p.getName() << " " << p.getDistance() << endl;
     }
+    // for(priority_queue<int>::iterator it = pq.begin(); it != pq.end(); i++)
+    // {
+    //     cout << *it << endl;
+    // }
     //2
+    cout << "-------1------" << endl;
     map<string, int> :: iterator current = shortest.begin();
     map<string, int> :: iterator stop = shortest.end();
     while (current != stop)
@@ -102,20 +120,69 @@ int main()
         cout << "Distance to " << p.first << " is " << p.second << "\n";
         ++current;
     }
-    // 2 & 3
-    map<string ,int>::iterator itr;
+    // 2 & 3 & 4
+    cout << "------2------" << endl;
+    map<string ,int>::iterator itr; //pair
     for(itr = shortest.begin(); itr != shortest.end(); itr++) 
         cout << itr->first << " " << itr->second << endl;
-    cout << "------------" << endl;
+    cout << "------3------" << endl;
 
+    for_each(shortest.begin(), shortest.end(), [](const std::map<string, int>::value_type &n){
+        cout << n.first <<" " << n.second << endl; //pair inside
+        });
+
+    cout << "------4-----" << endl;
+    
     for_each(shortest.begin(), shortest.end(), [](pair<string, int> n){
-      cout << n.first << " " << n.second << endl;
+      cout << n.first << " " << n.second << endl; //pair inside
     });
-    cout << "-----------" << endl;
+    cout << "------5-----" << endl;
+
     for_each(begin(shortest), end(shortest), [](const std::pair<string, int> &n){
-        cout << n.first <<" " << n.second << endl;
+        cout << n.first <<" " << n.second << endl; //pair inside
     });
 
+     cout << "------6-----" << endl;
+
+    for_each(begin(shortest), end(shortest), [](const unordered_map<string, int>::value_type &n){
+        cout << n.first <<" " << n.second << endl; //pair inside
+    });
+
+    cout << "------7-----" << endl;
+    vector<pair<string, int>> mapcopy(shortest.begin(), shortest.end());
+    sort(mapcopy.begin(), mapcopy.end(), less_second<string, int>());
+    for (auto n: mapcopy)
+    {
+        cout << n.first << " " << n.second << endl;
+    }
+    cout << "------8-----" << endl;
+    reverse(mapcopy.begin(), mapcopy.end());
+
+    for_each(mapcopy.begin(), mapcopy.end(), [](pair<string, int> n){
+      cout << n.first << " " << n.second << endl; //pair inside
+    });
+
+   // for_each(mapcopy.begin(), mapcopy.end(), [](pair<string, int>)
+
+    // for_each(mapcopy.begin(), mapcopy.end(), []( int n){
+    //     cout << n << endl;
+    // });
+    // for_each(pq(begin), pq(end), [](int x){
+    //     cout << x.first << endl; //pair inside
+    // });
+
+    // auto cmp = [](pair<string, int> const &a, pair<string, int> const &b)
+    // {
+    //     return a.second == b.second;
+    // }
+    //sort(shortest.begin(), shortest.end());
+    //vector<pair<int, string>> shortest;
+    //Sort using vector
+    
+    // sort(shortest.begin(), shortest.end(), [](const shortest &a, const shortest &b)->bool
+    // {
+    //     return a.shortest > b.shortest;
+    // });
     return 0;
 }
 
